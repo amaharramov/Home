4a363d335d8d92c85df70e4ba7e928864bd84310
Ali Maharramov <a.maharramovv@gmail.com>
2023-10-22 14:27:16 +0400

    Lesson3

--- js.js
+++ js.js
@@ -1,111 +1,56 @@
 'use strict';
+//1. Дан непустой массив чисел. Написать алгоритм (не исползьуя встроенные методы), который изменит исходный массив так, чтобы числа в нём шли в обратном порядке
+// const array = [1, 2, 3, 4, 5, 6, 7];
 
-//Регистрация (сложность 3)
+// for (let i = array.length; i--; ) {
+//   console.log(array[i]);
+// }
 
-// alert('Registration');
-// let login = prompt('Enter your login for registration');
-// let pass = prompt('Enter your password for registration');
-// alert('Authorization');
+//2. Дан массив и переменная с неким значением. Реализовать алгоритм встроенного метода unshift, не используя встроенные методы
 
-// while (true) {
-//   const newLogin = prompt('Enter your login for authorization');
-//   const newPass = prompt('Enter your password for authorization');
-//   console.log(newLogin, newPass);
-//   if (login === newLogin && pass === newPass) {
-//     alert('Valid login and password');
-//     break;
+// const arr = [2, 4, 6];
+// const element = 'begin';
+
+// for (let i = arr.length; i > 0; i--) {
+//   arr[i] = arr[i - 1];
+// }
+
+// arr[0] = element;
+// console.log(arr);
+
+//3. Дан некий объект и 10 попыток для ползователя, чтобы ввести названия полей этого объекта. Написать алгоритм, который по окончании выведет в консоль 2 массива:
+
+// const userProfile = {
+//   name: 'Ali',
+//   surname: 'Maharramov',
+//   age: 30,
+//   gender: 'male',
+//   country: 'Azerbaijan',
+//   city: 'Baku',
+// };
+
+// let data;
+// const arrExists = [];
+// const arrNotExists = [];
+// for (let i = 0; i < 10; i++) {
+//   data = prompt('Try ' + (i + 1) + '. Enter value : ').trim();
+//   if (
+//     userProfile.age == data ||
+//     userProfile.city == data ||
+//     userProfile.country == data ||
+//     userProfile.gender == data ||
+//     userProfile.name == data ||
+//     userProfile.surname == data
+//   ) {
+//     arrExists.push(data);
+//   } else if (data === '') {
+//     arrNotExists.push('null');
 //   } else {
-//     alert('Incorrect data');
+//     arrNotExists.push(data);
 //   }
 // }
 
-//Регистрация (сложность 4)
+// console.log(`Поля, которые были в объекте ${arrExists}`);
+// console.log(`Поля, которые не были в объекте ${arrNotExists}`);
 
-// alert('Registration');
-// let login = prompt('Enter your login for registration');
-// let pass = prompt('Enter your password for registration');
-// let limit = 3;
-// let attempts = 0;
 
-// while (true) {
-// alert('Authorization');
-//   const newLogin = prompt('Enter your login for authorization');
-//   const newPass = prompt('Enter your password for authorization');
-//   let createNewUser;
-//   console.log(newLogin, newPass);
-//   if (login === newLogin && pass === newPass) {
-//     alert('Valid login and password');
-//     break;
-//   } else {
-//     alert('Incorrect data');
-//     attempts++;
-//   }
-//   if (attempts === limit) {
-//     createNewUser = confirm('Would you like to create a new user?');
-//     if (createNewUser) {
-//       alert('Registration');
-//       login = prompt('Enter your login for registration');
-//       pass = prompt('Enter your password for registration');
-//       attempts = 0;
-//     } else {
-//       attempts = 0;
-//       alert('Type correct login and password');
-//     }
-//   }
-// }
-
-//Регистрация (сложность 5)
-
-alert('Registration');
-let login = prompt('Enter your login for registration');
-let pass = prompt('Enter your password for registration');
-let limit = 3;
-let attempts = 0;
-
-while (true) {
-  alert('Authorization');
-  const newLogin = prompt('Enter your login for authorization');
-  const newPass = prompt('Enter your password for authorization');
-  let createNewUser;
-  console.log(newLogin, newPass);
-  if (login === newLogin && pass === newPass) {
-    alert('Valid login and password');
-    break;
-  } else {
-    alert('Incorrect data');
-    attempts++;
-  }
-  if (attempts === limit) {
-    createNewUser = confirm('Would you like to create a new user?');
-    if (createNewUser) {
-      alert('Registration');
-      login = prompt('Enter your login for registration');
-      pass = prompt('Enter your password for registration');
-      attempts = 0;
-    } else {
-      while (true) {
-        let resetPass = confirm(' Do you want to reset your password ?');
-        if (resetPass) {
-          let resetLogin = prompt('Enter your login to reset your password.');
-          if (resetLogin === login) {
-            alert('Password has been reseted');
-            pass = prompt('Enter your NEW password');
-            break;
-          } else {
-            alert("Login doesn't match");
-            createNewUser = confirm('Would you like to create a new user?');
-            if (createNewUser) {
-              alert('Registration');
-              login = prompt('Enter your login for registration');
-              pass = prompt('Enter your password for registration');
-              attempts = 0;
-              break;
-            } else {
-              continue;
-            }
-          }
-        }
-      }
-    }
-  }
-}
